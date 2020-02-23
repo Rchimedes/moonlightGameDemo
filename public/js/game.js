b4db60d41b7f98dc73a22797e3e6cb153456f2b9
Lance Wade <seventh_fret1@yahoo.com>
2020-02-23 12:11:18 -0800

    Added files for project

---
+++ public/js/game.js
@@ -0,0 +1,172 @@
+var config = {
+    type: Phaser.AUTO,
+    parent: 'phaser-example',
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: false,
+            gravity: { y: 0 }
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload() {
+    this.load.image('ship', 'assets/spaceShips_001.png');
+    this.load.image('otherPlayer', 'assets/enemyBlack5.png');
+    this.load.image('star', 'assets/bolt_gold.png');
+    // Below was commented out to prevent lag. But for single player it works decently.
+    //TO DO: Need to find a way to be able to play song below without lag during multiplayer game.
+    // this.load.audio('dragonforce', 'audio/dragonforce1.mp3'); 
+    this.load.audio('coin', 'audio/powerup.mp3');
+
+    
+}
+
+function create() {
+    var self = this;
+    let coinGrab = this.sound.add('coin');
+    
+    this.socket = io();
+    this.cursors = this.input.keyboard.createCursorKeys();
+    this.otherPlayers = this.physics.add.group();
+    this.socket.on('currentPlayers', function (players) {
+        Object.keys(players).forEach(function (id) {
+            if (players[id].playerId === self.socket.id) {
+                addPlayer(self, players[id]);
+            } else {
+                addOtherPlayers(self, players[id]);
+            }
+        });
+    });
+    this.socket.on('newPlayer', function (playerInfo) {
+        addOtherPlayers(self, playerInfo);
+    });
+    this.socket.on('disconnect', function (playerId) {
+        self.otherPlayers.getChildren().forEach(function (otherPlayer) {
+            if (playerId === otherPlayer.playerId) {
+                otherPlayer.destroy();
+            }
+        });
+    });
+
+    this.socket.on('playerMoved', function (playerInfo) {
+        self.otherPlayers.getChildren().forEach(function (otherPlayer) {
+            if (playerInfo.playerId === otherPlayer.playerId) {
+                otherPlayer.setRotation(playerInfo.rotation);
+                otherPlayer.setPosition(playerInfo.x, playerInfo.y);
+            }
+        });
+    });
+
+    this.blueScoreText = this.add.text(16, 16, '', { fontSize: '32px', fill: '#0000FF' });
+    this.redScoreText = this.add.text(630, 16, '', { fontSize: '32px', fill: '#FF0000' });
+    this.gameOver = this.add.text (150,200,'', {fontSize: '60px', fill: '#FFFFFF' } )
+
+    this.socket.on('scoreUpdate', function (scores) {
+        self.blueScoreText.setText('Blue: ' + scores.blue);
+        self.redScoreText.setText('Red: ' + scores.red);
+    });
+
+    this.socket.on('starLocation', function (starLocation) {
+        if (self.star) self.star.destroy();
+        self.star = self.physics.add.image(starLocation.x, starLocation.y, 'star');
+        self.physics.add.overlap(self.ship, self.star, function () {
+            this.socket.emit('starCollected');
+            coinGrab.play();
+
+        }, null, self);
+    });
+
+    this.socket.on('gameover', function (teamWin) {
+        self.gameOver.setText(teamWin);
+        
+    })
+    
+    let backgroundMusic = this.sound.add('dragonforce', {volume: 0.5});
+    backgroundMusic.play();
+
+}
+
+function update() {
+    if (this.ship) {
+        if (this.cursors.left.isDown) {
+            this.ship.setAngularVelocity(-150);
+        } else if (this.cursors.right.isDown) {
+            this.ship.setAngularVelocity(150);
+        } else {
+            this.ship.setAngularVelocity(0);
+        }
+
+        if (this.cursors.up.isDown) {
+            this.physics.velocityFromRotation(this.ship.rotation + 1.5, 100, this.ship.body.acceleration);
+        } else {
+            this.ship.setAcceleration(0);
+        }
+
+        // emit player movement
+        var x = this.ship.x;
+        var y = this.ship.y;
+        var r = this.ship.rotation;
+        if (this.ship.oldPosition && (x !== this.ship.oldPosition.x || y !== this.ship.oldPosition.y || r !== this.ship.oldPosition.rotation)) {
+            this.socket.emit('playerMovement', { x: this.ship.x, y: this.ship.y, rotation: this.ship.rotation });
+        }
+
+        // save old position data
+        this.ship.oldPosition = {
+            x: this.ship.x,
+            y: this.ship.y,
+            rotation: this.ship.rotation
+        };
+
+        if(this.ship.x > 800) {
+            this.ship.x = 0;
+        }
+        
+        if(this.ship.y > 600) {
+            this.ship.y = 0;
+        }
+
+        if(this.ship.y < 0) {
+            this.ship.y = 600;
+        }
+
+        if(this.ship.x < 0) {
+            this.ship.x = 800;
+        }
+
+
+    }
+    // this.ship.physics.world.wrap(this.ship, 5);
+}
+
+function addPlayer(self, playerInfo) {
+    self.ship = self.physics.add.image(playerInfo.x, playerInfo.y, 'ship').setOrigin(0.5, 0.5).setDisplaySize(53, 40);
+    if (playerInfo.team === 'blue') {
+        self.ship.setTint(0x0000ff);
+    } else {
+        self.ship.setTint(0xff0000);
+    }
+    self.ship.setDrag(100);
+    self.ship.setAngularDrag(100);
+    self.ship.setMaxVelocity(200);
+}
+
+function addOtherPlayers(self, playerInfo) {
+    const otherPlayer = self.add.sprite(playerInfo.x, playerInfo.y, 'otherPlayer').setOrigin(0.5, 0.5).setDisplaySize(53, 40);
+    if (playerInfo.team === 'blue') {
+        otherPlayer.setTint(0x0000ff);
+    } else {
+        otherPlayer.setTint(0xff0000);
+    }
+    otherPlayer.playerId = playerInfo.playerId;
+    self.otherPlayers.add(otherPlayer);
+}
